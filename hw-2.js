46b061cfad2bb5ae824ef672f5d359eb62cfc4ec
alla <146801313+allvasin@users.noreply.github.com>
2024-02-16 00:08:24 +0300

    Добавлено mobile.scss и  hw2.js

---
+++ hw-2.js
@@ -0,0 +1,88 @@
+/*
+// Задание 1
+let a = 10;
+alert (a);
+a = 20;
+alert(a);
+
+// Задание 2
+let iphoneYear = 2007;
+alert(iphoneYear);
+
+// Задание 3
+let nameJS = "Brendan Eich";
+alert (nameJS);
+
+// Задание 4
+let var1 = 10, var2 = 2;
+alert(var1 + var2);
+alert(var1 - var2);
+alert(var1 * var2);
+alert(var1 / var2);
+
+// Задание 5
+let result =  2 ** 5;
+alert(result);
+
+
+// Задание 6
+let b = 2;
+a = 9;
+let mod = a % b;
+alert(mod);
+
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+// Задание 8
+let age = prompt("Сколько вам лет?")
+alert(age);*/
+
+// Задание 9.0
+let user = {
+    name: 'Alice',
+    age: 16,
+    isAdmin: true
+ }
+
+ // Задание 9.1
+ user ['city of residence'] = "Tokio";
+
+ // Задание 9.2
+ user.age = 18;
+
+ // Задание 9.3
+ //delete user ['city of residence'];
+
+ // Задание 9.4
+let info = prompt("Что вы хотите узнать о пользователе?", "name", "age", "isAdmin");
+// let info = prompt("Что вы хотите узнать о пользователе?", "city of residence");
+
+ alert(user[info]);
+
+ 
+/*
+ // Задание 10
+ let name = prompt('Как вас зовут?')
+ alert(`Привет,  ${name}!`)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
