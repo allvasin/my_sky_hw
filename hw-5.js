082974b25d3481343ae337ddc650c200f779ac5f
alla <146801313+allvasin@users.noreply.github.com>
2024-03-13 19:41:41 +0300

    Задания 1-4 домашки 5(функции)

---
+++ hw-5.js
@@ -0,0 +1,41 @@
+// Задание 1
+// function minNumber(num1, num2) {
+//     if (num1 > num2) return num2;
+//     else return num1;
+// };
+// console.log(minNumber(5,9));
+
+//const getMin = (a, b) => a < b ? a : b;
+
+// Задание 2
+// function numEven(num) {
+//     if (num % 2 === 0) {console.log('Число четное');}
+//     else console.log('Число нечетное'); 
+// }
+
+// Задание 3.1
+// function powTwo(num) {
+//     console.log(num ** 2);
+// }
+
+// задание 3.2
+// function powTwo_2(num) {
+//     return (num ** 2);
+// }
+
+// Задание 4
+// function helloFriend() {
+//     let age = prompt('Сколько вам лет?');
+//     age = Number(age);
+
+//     if (age < 0 || isNaN(age)) {
+//         alert('Вы ввели неправильное значение');
+//     }
+//     else if (age <= 12) {
+//         alert('Привет, друг!');
+//     }
+//     else {
+//         alert('Добро пожаловать!');
+//     }
+// }
+
