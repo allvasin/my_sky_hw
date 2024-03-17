5b3b01930a26ada4c0e16dd8bba59a7f6c7c409c
alla <146801313+allvasin@users.noreply.github.com>
2024-03-17 17:52:11 +0300

    Задачи 1-6 дз6 (массивы)

---
+++ hw-6.js
@@ -0,0 +1,55 @@
+ // let numbers = [1, 5, 4, 10, 0, 3];
+
+// Задание 1
+// for (let i = 0; i <= numbers.length; i ++) {
+//     console.log(numbers[i]);
+//     if (numbers[i] === 10) {   
+//         break;
+//     }   
+// }
+
+// Задание 2
+//console.log(numbers.indexOf(4));
+
+// Задание 3
+// const arr = [1, 3, 5, 10, 20];
+// console.log(arr.join(' '));
+//console.log(arr.join(''));
+
+// Задание 4
+// let newArray = [];
+// //let m = 1
+// for (let j = 0; j < 3; j ++){
+//     newArray[j] = [];
+   
+//     for(let k = 0; k < 3; k ++){
+//        // newArray[j].push(m);
+//         newArray[j].push(1);
+//        // m ++;
+//     }
+// }
+// console.log(newArray);
+
+// Задание 5
+// const arr1 = [1, 1, 1];
+// arr1.push(2, 2, 2);
+// console.log(arr1);
+
+// Задание 6
+// let arr2 = [9, 8, 7, 'a', 6, 5];
+// arr2.sort().pop();
+// console.log(arr2);
+
+// Задание 7
+
+
+
+
+
+// Задание 8
+
+
+
+
+
+
